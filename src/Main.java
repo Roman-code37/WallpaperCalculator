c8f68625fe2ba7877a78604a64e46e38dc36c41e
Roman <groma_92@mail.ru>
2019-09-18 17:15:37 +0300

    Initials commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int length = 5 * 2;
+        int width = 3 * 2;
+        float heigth = 2.5F;
+        int roll = 1;
+        int length2 = 10;
+        int perimetr = (length) + (width);
+        int sum = perimetr / roll;
+        float quantity = length2 / heigth;
+        float total = 1 + sum / quantity;
+        System.out.println(total);
+
+    }
+}
