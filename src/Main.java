f556200a27f568757a1f5cbc12d758b8d9ac0cc8
Roman <groma_92@mail.ru>
2019-09-21 02:54:36 +0300

    .gitignore add

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        int length = 5 * 2;
-        int width = 3 * 2;
+        byte length = 5;
+        byte width = 3;
         float heigth = 2.5F;
-        int roll = 1;
-        int length2 = 10;
-        int perimetr = (length) + (width);
+        byte roll = 1;
+        byte rollLength = 10;
+        int perimetr = (length + width) * 2;
         int sum = perimetr / roll;
-        float quantity = length2 / heigth;
+        float quantity = rollLength / heigth;
         float total = 1 + sum / quantity;
         System.out.println(total);
 
